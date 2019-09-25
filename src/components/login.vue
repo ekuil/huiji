a9d0a8c41900208667637d00535a5c42aaac5cb3
ekuil <liuked163@163.com>
2019-09-25 09:30:47 +0800

    Add files via upload
    init

---
+++ src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{msg}}
+        用户名:<input type="text">
+        密码:<input type="password">
+    </div>
+</template>
+<script>
+export default {
+    name:'login',
+   data(){
+       return {
+        msg:'--------------------系统'
+       }
+   }
+}
+</script>
