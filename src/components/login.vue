69967e898d2f3117db0b960bef40ba853fb3d52d
liuke <837213832@qq.com>
2020-07-26 21:30:35 +0800

    update login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         {{msg}}
-        <el-input v-model="a" placeholder="账号"></el-input>
-        <el-input v-model="b" placeholder="密码"></el-input>
+        <el-input v-model="input1" placeholder="账号"></el-input>
+        <el-input v-model="input2" placeholder="密码" show-password minlength="6"> </el-input>
       <el-button  type="primary" @click.native.prevent="login">登录</el-button>
     </div>
 </template>
@@ -11,13 +11,16 @@ export default {
     name:'login',
    data(){
        return {
-        msg:'--------------------系统'
+        msg:'101 FAMILY',
+        input1:"",
+        input2:""
+
        }
    },
     methods:{
         login(){
-            let uinfo={u:this.a,p:this.b}
-            alert("33"+uinfo)
+            let uinfo={u:this.input1,p:this.input2}
+            alert("33"+uinfo.p)
         }
     }
 }
